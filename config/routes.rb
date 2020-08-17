1b01d1a592027408ae05f0bbe1aee5275cb22c9f
github username <joshmrallen@gmail.com>
2020-08-17 12:10:22 -0400

    add blank models, controllers, initial routes, and migration files

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
+  root '/login' #come back to this
+  resources :favorites
+  resources :quotes
+  resources :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
